3849e404366433086272d1778a2c4465395a8c9f
ImbangJayoGampito <146147505+ImbangJayoGampito@users.noreply.github.com>
2024-11-25 00:30:54 +0700

    fixed collision

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,14 +3,16 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    Rigidbody2D rb;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+      
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
