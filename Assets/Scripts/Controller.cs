f2fb6efc6f178e4599238ab84f92c5f359a9367f
ImbangJayoGampito <146147505+ImbangJayoGampito@users.noreply.github.com>
2024-11-28 16:00:26 +0700

    tambah sistem movement
    it sucks

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,37 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+
+public class Controller : MonoBehaviour
+{
+    public KeyCode jump = KeyCode.W;
+    public KeyCode left = KeyCode.A;
+    public KeyCode right = KeyCode.D;
+    public KeyCode crouch = KeyCode.S;
+    public KeyCode sprint = KeyCode.LeftShift;
+    public MouseButton attack = MouseButton.Left;
+    private static Controller instance;
+    public static Controller Instance { get { return instance; } }
+    private void Awake()
+    {
+        // If there is an instance, and it's not me, delete myself.
+
+        if (instance != null && instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+    private void OnDestroy()
+    {
+        // Clear the instance if this instance is destroyed
+        if (instance == this)
+        {
+            instance = null;
+        }
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,131 @@
+using System;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     Rigidbody2D rb;
+
+    public float jumpForce;
+    public float speed;
+    public float acceleration;
+    public float friction;
+    private float currentSpeed = 0.0f;
+    private bool grounded;
+    private Controller controller;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-      
+        controller = Controller.Instance;
+        Debug.Log(controller.jump);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        float speedCap = speed;
+
+        if (Input.GetKey(controller.sprint))
+        {
+            speedCap = speed * 2;
+        }
+        if (Input.GetKey(controller.left))
+        {
+
+            currentSpeed = -speedCap;
+
+
+        }
+        if (Input.GetKey(controller.right))
+        {
+
+            currentSpeed = speedCap;
+
+
+
+        }
+        bool isNegative = currentSpeed < 0;
+        float reduceSpeed = (isNegative) ? -friction : friction;
+        if (!grounded)
+        {
+            reduceSpeed /= 2;
+        }
+        Debug.Log(Math.Abs(currentSpeed - reduceSpeed * Time.deltaTime));
+        if (Math.Abs(currentSpeed - reduceSpeed * Time.deltaTime) <= 0)
+        {
+            currentSpeed = 0;
+        }
+        else
+        {
+            currentSpeed -= reduceSpeed * Time.deltaTime;
+        }
+        rb.linearVelocityX = currentSpeed;
+        if (Input.GetKey(controller.jump))
+        {
+            if (grounded)
+            {
+                rb.linearVelocityY = jumpForce;
+            }
+
+
+        }
+
+    }
+    private enum HitDirection { None, Top, Bottom, Forward, Back, Left, Right };
+    private HitDirection GetDirection(GameObject ObjectHit)
+    {
+
+        HitDirection hitDirection = HitDirection.None;
+        RaycastHit MyRayHit;
+        Vector3 direction = (gameObject.transform.position - ObjectHit.transform.position).normalized;
+        Ray MyRay = new Ray(ObjectHit.transform.position, direction);
+
+        if (Physics.Raycast(MyRay, out MyRayHit))
+        {
+
+            if (MyRayHit.collider != null)
+            {
+
+                Vector3 MyNormal = MyRayHit.normal;
+                MyNormal = MyRayHit.transform.TransformDirection(MyNormal);
+
+                if (MyNormal == MyRayHit.transform.up) { hitDirection = HitDirection.Top; }
+                if (MyNormal == -MyRayHit.transform.up) { hitDirection = HitDirection.Bottom; }
+                if (MyNormal == MyRayHit.transform.forward) { hitDirection = HitDirection.Forward; }
+                if (MyNormal == -MyRayHit.transform.forward) { hitDirection = HitDirection.Back; }
+                if (MyNormal == MyRayHit.transform.right) { hitDirection = HitDirection.Right; }
+                if (MyNormal == -MyRayHit.transform.right) { hitDirection = HitDirection.Left; }
+            }
+        }
+        return hitDirection;
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+
+    {
+        Debug.Log(GetDirection(collision.collider.gameObject));
+        // Check if the collided object has the "Collidable" tag
+
+        if (collision.collider.CompareTag("Collidable"))
+
+        {
+            currentSpeed = 0.0f;
+            grounded = true;
+
+        }
+    }
+
+
+    void OnCollisionExit2D(Collision2D collision)
+
+    {
+
+        // Check if the collided object has the "Collidable" tag
+
+        if (collision.collider.CompareTag("Collidable"))
+
+        {
+            grounded = false;
+        }
 
     }
 }
